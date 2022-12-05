d9f0c1c27672c443d312821442d53ab34e185ddc
Alejandro <Alekgy@gmail.com>
2022-12-05 18:31:22 -0500

    add db port

--- CRUD_Django/settings.py
+++ CRUD_Django/settings.py
@@ -85,12 +85,9 @@ WSGI_APPLICATION = 'CRUD_Django.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',   
+        default='postgresql://postgres:postgres@localhost/postgres',   
         conn_max_age=600
     ) 
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    
 }
 
 
